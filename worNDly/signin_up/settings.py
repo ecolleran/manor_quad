78cc9b5073193d4111b362241728b07f43be0491
Emily Colleran <ecoller2@student05.cse.nd.edu>
2024-04-24 22:47:33 -0400

    delete settings file bc its for blog

--- worNDly/signin_up/settings.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-app_name = ‘signin_up’  # creates a namespace for this app
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:post_id>/', views.view_post, name='view_post'),
-
-]
