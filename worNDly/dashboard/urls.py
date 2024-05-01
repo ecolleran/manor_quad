7bdc36b765627c7b9ab8fdfbdeefae76439838a9
Zachary Sellman <zsellman@student05.cse.nd.edu>
2024-05-01 02:43:58 -0400

    Percentage

--- worNDly/dashboard/urls.py
+++ worNDly/dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='dashboard'
 urlpatterns = [
     path('open_dashboard/', views.open_dashboard, name='open_dashboard'),
+    path('show_statistics/', views.show_statistics, name='show_statistics')
 ]
--- worNDly/dashboard/views.py
+++ worNDly/dashboard/views.py
@@ -3,38 +3,45 @@ from django.contrib.auth.models import User
 from gameplay.models import GamesPlayed
 import datetime
 
-# Create your views here.
-
 def open_dashboard(request):
+    selected_data_view = request.GET.get('data-view', 'all')  # Get selected data view from the form
+
     listOfPlays = []
     listOfPlaysYEAR = []
     listOfPlaysMONTH = []
     listOfPlaysWEEK = []
 
-
     for g in GamesPlayed.objects.all():
         if request.user == g.player:
-            print(g)
             listOfPlays.append(g)
         
         if request.user == g.player and g.game_play_date.year == datetime.datetime.now().year:
-            print(g)
             listOfPlaysYEAR.append(g)
         
         if request.user == g.player and g.game_play_date.month == datetime.datetime.now().month:
-            print(g)
             listOfPlaysMONTH.append(g)
         
         if request.user == g.player and g.game_play_date.isocalendar()[1] == datetime.datetime.now().isocalendar()[1]:
-            print(g)
             listOfPlaysWEEK.append(g)
-        
-        
-        
-        
-        
 
-        
+    # Pass the selected data view and relevant data to the template
+    return render(request, 'dashboard/firstdash.html', {
+        'selected_data_view': selected_data_view,
+        'listOfPlays': listOfPlays,
+        'listOfPlaysYEAR': listOfPlaysYEAR,
+        'listOfPlaysMONTH': listOfPlaysMONTH,
+        'listOfPlaysWEEK': listOfPlaysWEEK
+    })
 
+def show_statistics(request):
+    tot = 0
+    succ = 0
+    for g in GamesPlayed.objects.filter(player = request.user):
+        tot += 1
+        if g.won_or_nah:
+            succ += 1
+    
+    perc = (succ / tot) * 100
 
-    return render(request, 'dashboard/firstdash.html', {'listOfPlays': listOfPlays, 'listOfPlaysYEAR': listOfPlaysYEAR, 'listOfPlaysMONTH': listOfPlaysMONTH, 'listOfPlaysWEEK': listOfPlaysWEEK})
+    
+    return render(request, 'dashboard/stats.html', {'perc': str(perc) + '%'})
