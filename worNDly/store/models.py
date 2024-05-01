560d5efe981eea2eeebbc8de70baee2e72709627
Emily Colleran <ecoller2@student10.cse.nd.edu>
2024-05-01 01:33:33 -0400

    need my store model to work

--- worNDly/store/models.py
+++ worNDly/store/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-# from signin_up.models import Player
+from django.contrib.auth.models import User
 
 # Create your models here.
-'''
+
 class Wallet(models.Model):
-    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+    player = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.IntegerField(default=0)
 
     def __str__(self):
         return self.player.username
-'''
