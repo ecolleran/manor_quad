4106c9f16bbd29a0ef2268592cedffd8379c9e4f
Timothy Weber <tweber4@student13.cse.nd.edu>
2024-05-01 19:51:20 -0400

    added store, not quite done yet

--- worNDly/store/urls.py
+++ worNDly/store/urls.py
@@ -1,7 +1,11 @@
+# urls.py
+
 from django.urls import path
 from . import views
 
-app_name='store'
+app_name = 'store'
+
 urlpatterns = [
-    path('purchase_games/', views.purchase_games, name='purchase_games'),
+    path('purchase/', views.purchase_games, name='purchase_games'),
+    # Other URL patterns go here
 ]
